4c31b5a22f9a4a1c265e5b4de8cf39f808bce8e9
Ziad Hatem <ziadhatem2022@gmail.com>
2024-08-11 16:27:51 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <ul className="flex flex-col gap-4 m-4 w-full items-center justify-center h-screen">
+      <ul className="flex flex-col gap-4 w-screen items-center justify-center h-screen">
         <li className="bg-gray-200 p-4 rounded-md">
           <Link href="/by_seatingNo" className="text-xl font-bold">
             البحث برقم الجلوس
